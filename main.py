573dc1efa1af2d36d81e1ffe74e55ca25ca295b8
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 23:12:13 +0700

    Introduction to Python try…catch…finally statement

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 if __name__ == "__main__":
-  print("Python try…except…finally")
+  # Introduction to Python try…catch…finally statement
+
+  """
+  try:
+    # code that may cause exceptions
+  except:
+    # code that handle exceptions
+  finally:
+    # code that clean up
+  """
